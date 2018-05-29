5ec42172e80fc41ff25bb34e4e44021599a19c69
Dariusz Maciejewski <darek.maciejewski@snow.dog>
2018-05-29 12:43:50 +0200

    Change variable name

--- app/code/community/Snowdog/CreditagricoleRaty/controllers/InfoController.php
+++ app/code/community/Snowdog/CreditagricoleRaty/controllers/InfoController.php
@@ -10,7 +10,7 @@ class Snowdog_CreditagricoleRaty_InfoController extends Mage_Core_Controller_Fro
         $request = $this->getRequest();
         $productId = $request->getParam('id');
         $price = $request->getParam('price');
-        $selection = $request->getParam('optionIds');
+        $selections = $request->getParam('optionIds');
 
         $product = Mage::getModel('catalog/product')->load($productId);
         if (!$product->getId() || $product->getTypeId() !== Mage_Catalog_Model_Product_Type::TYPE_BUNDLE) {
@@ -28,12 +28,12 @@ class Snowdog_CreditagricoleRaty_InfoController extends Mage_Core_Controller_Fro
         $block->setProductId($productId);
 
         $selectionQtys = array();
-        $selection = array_filter(explode(',', $selection));
-        foreach ($selection as $item) {
-            if (substr_count($item, '-') !== 1) {
+        $selections = array_filter(explode(',', $selections));
+        foreach ($selections as $selection) {
+            if (substr_count($selection, '-') !== 1) {
                 continue;
             }
-            list($selectionId, $qty) = explode('-', $item, 2);
+            list($selectionId, $qty) = explode('-', $selection, 2);
             $selectionQtys[(int) $selectionId] = (int) $qty;
         }
         $block->setSelectionQtys($selectionQtys);
