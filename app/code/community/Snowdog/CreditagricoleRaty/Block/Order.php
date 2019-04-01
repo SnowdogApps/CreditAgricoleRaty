84243ce2164abc89f9cec8c5c18a012d78694592
Marek Jasiukiewicz <marek@snow.dog>
2019-04-01 22:07:49 +0200

    Unlock a link to re-send the application for guest customers

--- app/code/community/Snowdog/CreditagricoleRaty/Block/Order.php
+++ app/code/community/Snowdog/CreditagricoleRaty/Block/Order.php
@@ -8,16 +8,13 @@ class Snowdog_CreditagricoleRaty_Block_Order extends Mage_Payment_Block_Info {
 	public function _toHtml() {
 		$msg = Mage::helper('snowcreditagricoleraty')->__("Płatnośc ratalna Credit Agricole Raty.");
 		if ($this->getInfo()->getOrder()) {
-			$info = $this->getInfo();
 			$order = $this->getInfo()->getOrder();
 			/* @var $order Mage_Sales_Model_Order */
 			$id = $order->getIncrementId();
-			if (!$order->getCustomerIsGuest()) {
-				$msg .= "\n\r<br />";
-				$msg .= "<a href='" . $this->getUrl('snowcreditagricoleraty/payment/submit', array('order' => $id)) . "'>";
-				$msg .= Mage::helper('snowcreditagricoleraty')->__("Złóż wniosek ratalny.");
-				$msg .= "</a>";
-			}
+            $msg .= "\n\r<br />";
+            $msg .= "<a href='" . $this->getUrl('snowcreditagricoleraty/payment/submit', array('order' => $id)) . "'>";
+            $msg .= Mage::helper('snowcreditagricoleraty')->__("Złóż wniosek ratalny.");
+            $msg .= "</a>";
 		}
 		return $msg;
 	}
